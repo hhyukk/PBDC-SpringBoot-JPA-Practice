884d9f5243a83ebd6b1e811719624918411dcdf3
hhyukk <hhyukk1273@gmail.com>
2026-01-26 01:10:50 +0900

    refactor: Auditing 공통 필드를 BaseEntity로 분리

--- src/main/java/com/back/domain/member/member/entity/Member.java
+++ src/main/java/com/back/domain/member/member/entity/Member.java
@@ -1,34 +1,18 @@
 package com.back.domain.member.member.entity;
 
+import com.back.global.jpa.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
-
-import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 @NoArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
-public class Member {
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    private int id;
-    @CreatedDate
-    private LocalDateTime createDate;
-    @LastModifiedDate
-    private LocalDateTime modifyDate;
+public class Member extends BaseEntity {
     @Column(unique = true)
     private String username;
     private String password;
     private String nickname;
 
     public Member(String username, String password, String nickname) {
-        this.createDate=LocalDateTime.now();
-        this.modifyDate=LocalDateTime.now();
         this.username=username;
         this.password=password;
         this.nickname=nickname;
