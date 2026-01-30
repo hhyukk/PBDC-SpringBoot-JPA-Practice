d0be38554f01666276e468adb12c1bf029af4215
hhyukk <hhyukk1273@gmail.com>
2026-01-30 21:17:38 +0900

    feat: 게시글 댓글(PostComment) 도메인 추가 및 연관관계 구현

--- src/main/java/com/back/global/BaseinitData.java
+++ src/main/java/com/back/global/BaseinitData.java
@@ -50,18 +50,42 @@ public class BaseinitData {
     public void work2() {
 
         if (postService.count() > 0) return;
-        Member userMember = memberService.findByUsername("user1").get();
+        Member user1Member = memberService.findByUsername("user1").get();
         Member user2Member = memberService.findByUsername("user2").get();
         Member user3Member = memberService.findByUsername("user3").get();
 
-        Post post1 = postService.write(userMember, "제목1", "내용1");
+        Post post1 = postService.write(user1Member, "제목1", "내용1");
+        Post post2 = postService.write(user1Member, "제목2", "내용2");
+        Post post3 = postService.write(user1Member, "제목3", "내용3");
+        Post post4 = postService.write(user2Member, "제목4", "내용4");
+        Post post5 = postService.write(user2Member, "제목5", "내용5");
+        Post post6 = postService.write(user3Member, "제목6", "내용6");
     }
 
     @Transactional
     public void work3() {
         Post post1 = postService.findById(1).get();
+
+        if (post1.hasComments()) return;
+
         Post post2 = postService.findById(2).get();
         Post post3 = postService.findById(3).get();
         Post post4 = postService.findById(4).get();
+
+        Member user1Member = memberService.findByUsername("user1").get();
+        Member user2Member = memberService.findByUsername("user2").get();
+        Member user3Member = memberService.findByUsername("user3").get();
+
+        post1.addComment(user1Member, "댓글1");
+        post1.addComment(user2Member, "댓글2");
+        post1.addComment(user3Member, "댓글3");
+
+        post2.addComment(user2Member, "댓글4");
+        post2.addComment(user2Member, "댓글5");
+
+        post3.addComment(user3Member, "댓글6");
+        post3.addComment(user3Member, "댓글7");
+
+        post4.addComment(user1Member, "댓글8");
     }
 }
