da613991187a656c5dee6df8c896089794c4f5b5
hhyukk <hhyukk1273@gmail.com>
2026-01-31 11:24:35 +0900

    feat: PostChain 도메인 추가 및 초기 데이터 시딩 로직 연동

---
+++ src/main/java/com/back/domain/post/postChain/service/PostChainService.java
@@ -0,0 +1,24 @@
+package com.back.domain.post.postChain.service;
+
+import com.back.domain.member.member.entity.Member;
+import com.back.domain.post.postChain.entity.PostChain;
+import com.back.domain.post.postChain.repository.PostChainRepository;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostChainService {
+    private final PostChainRepository postChainRepository;
+
+    public long count() {
+        return postChainRepository.count();
+    }
+
+    public PostChain make(Member author, String subject) {
+        PostChain postChain = new PostChain(author, subject);
+
+        return postChainRepository.save(postChain);
+    }
+}
